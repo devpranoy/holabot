1d644bcecc4d9de637ea05c5330672a68f3a1031
pranoy <pranoy@tyko.co>
2017-02-09 17:25:33 +0530

    user response news

--- app.py
+++ app.py
@@ -38,6 +38,8 @@ def webhook():
                     sender_id = messaging_event["sender"]["id"]        # the facebook ID of the person sending you the message
                     recipient_id = messaging_event["recipient"]["id"]  # the recipient's ID, which should be your page's facebook ID
                     message_text = messaging_event["message"]["text"]  # the message's text
+                    if message_text == "news":
+                        send_message(sender_id,"Message recieved")
 
                     send_message(sender_id,"no "+message_text )
 
