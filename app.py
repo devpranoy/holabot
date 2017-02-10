afff3c659ab274e2933711ab326ba7d26d23edc5
pranoy <pranoy@tyko.co>
2017-02-11 03:06:45 +0530

    user response news and hola cap to lower case and help button respond

--- app.py
+++ app.py
@@ -38,13 +38,14 @@ def webhook():
                     sender_id = messaging_event["sender"]["id"]        # the facebook ID of the person sending you the message
                     recipient_id = messaging_event["recipient"]["id"]  # the recipient's ID, which should be your page's facebook ID
                     message_text = messaging_event["message"]["text"]# the message's text
+                    postback_text="news0"
                     try:
                         postback_text= messaging_event["postback"]["payload"]#postbacks from buttons
                     except:
                         print "lol"
                     message_text= message_text.lower()
                     try:
-                        if postback_text=="news":
+                        if postback_text =="news":
                             news(sender_id)
                             break;
                     except:
