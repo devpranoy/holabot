4f9088366f4fc8c9f8174ed949cd282152ad1b94
devpranoy <pranoy@tyko.co>
2017-02-17 19:46:19 +0530

    Db user data management final test for broadcast

--- app.py
+++ app.py
@@ -74,7 +74,7 @@ def webhook():
                         send_message(sender_id,"createtable command was inintialised")
                         make_table(sender_id)
                         break;
-                    if message_text=="delete_table":
+                            #if message_text=="delete_table":
                         send_message(sender_id,"deletetable command was inintialised")
                         delete_table(sender_id)
                         break;
